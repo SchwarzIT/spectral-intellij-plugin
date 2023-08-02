75c753b6377453fb09ece753a5512d50300cfc29
Mark Brockhoff <mark.brockhoff@mail.schwarz>
2023-08-02 17:58:16 +0200

    Delete unused constructor from SpectralException

--- src/main/kotlin/com/schwarzit/spectralIntellijPlugin/SpectralException.kt
+++ src/main/kotlin/com/schwarzit/spectralIntellijPlugin/SpectralException.kt
@@ -2,6 +2,5 @@ package com.schwarzit.spectralIntellijPlugin
 
 class SpectralException : Exception {
     constructor(message: String) : super(message)
-    constructor(cause: Throwable) : super(cause)
     constructor(message: String, cause: Throwable) : super(message, cause)
 }
